18cf6d1a2b3a24dd5b94de5c2ed0455c2f6be20a
ayatocraft <ayatocraft1221@gmail.com>
2026-02-25 07:38:35 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,14 @@
+self.addEventListener("install", event => {
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("notificationclick", event => {
+    event.notification.close();
+    event.waitUntil(
+        clients.openWindow("https://ayatocraft.github.io/nexis-chat/")
+    );
+});
