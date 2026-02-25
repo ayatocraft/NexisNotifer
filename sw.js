5172f33df9af53911d61a5677b6e1d693a681eb4
ayatocraft <ayatocraft1221@gmail.com>
2026-02-25 20:45:37 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,9 @@
-self.addEventListener("install", event => {
-    self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-    event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener("notificationclick", event => {
-    event.notification.close();
-    event.waitUntil(
-        clients.openWindow("https://ayatocraft.github.io/nexis-chat/")
-    );
-});
 self.addEventListener("notificationclick", event=>{
 
     event.notification.close();
 
-    const url = event.notification.data?.url ||
-                "https://ayatocraft.github.io/NexisChat/";
+    const url = event.notification.data?.url
+        || "https://ayatocraft.github.io/NexisChat/";
 
     event.waitUntil(
         clients.matchAll({type:"window"}).then(clientList=>{
